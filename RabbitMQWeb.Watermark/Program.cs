c5355f08cf2df9cdfa4e24bec20de7d4d0c9ffbd
Cevdet Sedef <cevdettsedeff@gmail.com>
2023-11-01 23:31:32 +0300

    Watermark was added to the images with RabbitMQ.

--- RabbitMQWeb.Watermark/Program.cs
+++ RabbitMQWeb.Watermark/Program.cs
@@ -1,7 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Plugins;
+using RabbitMQ.Client;
+using RabbitMQWeb.Watermark.BackgroundServices;
+using RabbitMQWeb.Watermark.Models;
+using RabbitMQWeb.Watermark.Services;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton(sp => new ConnectionFactory()
+{
+    Uri = new Uri(builder.Configuration.GetConnectionString("RabbitMQ")),
+    DispatchConsumersAsync = true
+});
+builder.Services.AddHostedService<ImageWatermarkProcessBackgroundService>();
+builder.Services.AddSingleton<RabbitMQClientService>();
+builder.Services.AddSingleton<RabbitMQPublisher>(); //Tek bir nesne örneði yeterli olacaktýr.
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseInMemoryDatabase(databaseName: "ProductDb");
+});
+
+
 
 var app = builder.Build();
 
---
+++ RabbitMQWeb.Watermark/Services/RabbitMQPublisher.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Drawing.Printing;
+using System.Text;
+using System.Text.Json;
+
+namespace RabbitMQWeb.Watermark.Services
+{
+    public class RabbitMQPublisher
+    {
+        private readonly RabbitMQClientService _rabbitMQClientService;
+
+        public RabbitMQPublisher(RabbitMQClientService rabbitMQClientService)
+        {
+            _rabbitMQClientService = rabbitMQClientService;
+        }
+
+        public void Publish(productImageCreatedEvent _productImageCreatedEvent)
+        {
+            var channel = _rabbitMQClientService.Connect();
+
+            var bodyString = JsonSerializer.Serialize(_productImageCreatedEvent);
+
+            var bodyByte = Encoding.UTF8.GetBytes(bodyString);
+
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true; // verileri kalıcı hale getirdik.
+
+            channel.BasicPublish(exchange: RabbitMQClientService.ExchangeName, routingKey: RabbitMQClientService.RoutingWatermark, basicProperties: properties, body: bodyByte);
+        }
+    }
+}
